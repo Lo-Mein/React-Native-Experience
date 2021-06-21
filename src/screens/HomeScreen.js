4eded2609145a27f615564967d181aea11633141
Ryan <ryan.lo@fairfield.edu>
2021-06-20 23:36:52 -0400

    Ryans Changes

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,30 +1,45 @@
-import React from "react";
-import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
+import React from 'react';
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
-const HomeScreen = ({navigation}) => {
-
-  return (
-    <View>
-      <Text style={styles.text}>Hi there!!!!</Text>
-      <Button 
-      onPress={ () => navigation.navigate('Components')}
-      title="Go to components demo" 
-      />
-      <Button
-        onPress={() => navigation.navigate('List')}
-        title="Go to List Demo"
-      />
-      {/* <TouchableOpacity onPress={ () =>props.navigation.navigate("List")}>
+const HomeScreen = ({ navigation }) => {
+	return (
+		<View>
+			<Text style={styles.text}>Hi there!!!!</Text>
+			<Button
+				onPress={() => navigation.navigate('Components')}
+				title="Go to components demo"
+			/>
+			<Button
+				onPress={() => navigation.navigate('List')}
+				title="Go to List Demo"
+			/>
+			<Button
+				onPress={() => navigation.navigate('Image')}
+				title="Go to Image Demo"
+			/>
+			<Button
+				onPress={() => navigation.navigate('Counter')}
+				title="Go to Counter Screen"
+			/>
+			<Button
+				onPress={() => navigation.navigate('Color')}
+				title="Go to Color Demo"
+			/>
+			<Button
+				onPress={() => navigation.navigate('Square')}
+				title="Go to Square Demo"
+			/>
+			{/* <TouchableOpacity onPress={ () =>props.navigation.navigate("List")}>
         <Text>Go to List Demo</Text>
       </TouchableOpacity> */}
-    </View>
-  );
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 30
-  }
+	text: {
+		fontSize: 30,
+	},
 });
 
 export default HomeScreen;
